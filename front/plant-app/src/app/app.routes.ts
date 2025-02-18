fb472ba13bd1b31f93d25b7efc8aa145b99b80b2
Bouchra Laghouati <laghouatibouchra@hotmail.com>
2025-02-18 19:01:10 +0100

    FIX routes

--- front/plant-app/src/app/app.routes.ts
+++ front/plant-app/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import {AboutusComponent} from './aboutus/aboutus.component';
-import {HomeComponent} from './home/home.component';
-import {IndoorplantsComponent} from './indoorplants/indoorplants.component';
-import {OutdoorplantsComponent} from './outdoorplants/outdoorplants.component';
-
-
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { IndoorplantsComponent } from './indoorplants/indoorplants.component';
+import { OutdoorplantsComponent } from './outdoorplants/outdoorplants.component';
 
 export const routes: Routes = [
-    {path: 'aboutus-component', component: AboutusComponent},
-    {path: 'home-component', component: HomeComponent},
-    {path: 'indoorplants-component', component: IndoorplantsComponent},
-    {path: 'outdoorplants-component', component: OutdoorplantsComponent},
+    { path: 'aboutus-component', component: AboutusComponent },
+    { path: '', component: HomeComponent },
+    { path: 'indoorplants-component', component: IndoorplantsComponent },
+    { path: 'outdoorplants-component', component: OutdoorplantsComponent },
 ];
